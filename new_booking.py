aa977bc16d8bc2d43fe0d451e2f47615f3eed309
nak <pich_ratanak@ymail.com>
2019-03-08 11:39:38 +1100

    init commit

---
+++ new_booking.py
@@ -0,0 +1,104 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from random import randint
+import datetime
+import uuid
+
+phoneNumber = str(randint(10000000, 99999999))
+driver = webdriver.Chrome(executable_path=r"./chromedriver")
+driver.get("https://beta.urbanyou.com.au")
+
+def clickXPath(xpath):
+    driver.find_element_by_xpath(xpath).click()
+    time.sleep(0.5)
+
+def sendKeyXPath(key, xpath):
+    element = driver.find_element_by_xpath(xpath)
+    element.clear()
+    element.send_keys(key)
+
+#select book now
+driver.find_element_by_id('btn-book-now-header').click()
+time.sleep(0.5)
+
+#select cleaning
+clickXPath('//*[@id="icons"]/div[1]/a/div')
+
+#select end of least
+clickXPath('//*[@id="icons"]/div[3]/a/div')
+time.sleep(1)
+
+#type in postcode
+postcode = driver.find_element_by_id('cart_postcode')
+postcode.clear()
+postcode.send_keys('2000')
+
+#type in email
+email = driver.find_element_by_id('cart_email')
+email.clear()
+email.send_keys(phoneNumber+"@nakbot.com")
+time.sleep(0.5)
+
+#select GET PRICE
+clickXPath('//*[@id="cart"]/div/div/div[6]/div/button')
+
+#select NEXT
+clickXPath('//*[@id="booking"]/div/div[1]/div[2]/div/form/div[4]/div/button')
+
+#toggle calendar
+clickXPath('//*[@id="start_at"]')
+
+#select tomorrow date
+tomorrow = datetime.date.today() + datetime.timedelta(days=1)
+calendar = driver.find_element_by_xpath('//*[@id="booking"]/div/div[1]/div[2]/div/form/div[1]/div/div[1]/div/div[2]/div')
+calendarSpan = calendar.find_elements_by_xpath('//span')
+tomorrowSpan = filter(lambda x: x.text == str(tomorrow.day), calendarSpan)[0]
+index = str(calendarSpan.index(tomorrowSpan) - 13) #13 is any span before 1st date
+clickXPath('//*[@id="booking"]/div/div[1]/div[2]/div/form/div[1]/div/div[1]/div/div[2]/div/span['+index+']')
+
+#select start time
+clickXPath('//*[@id="start_time"]/option[4]')
+
+#type in address
+address = driver.find_element_by_xpath('//*[@id="property_address"]')
+address.send_keys('120 Sussex Street, SYDNEY NSW 2000')
+time.sleep(2)
+clickXPath('/html/body/ul/li[1]')
+
+#select property access
+clickXPath('//*[@id="access-row"]/div[1]/div/div/div[1]/label')
+
+#click NEXT
+clickXPath('//*[@id="booking"]/div/div[1]/div[2]/div/form/div[2]/div/button')
+time.sleep(1)
+
+#fill in client detail
+sendKeyXPath('Nak', '//*[@id="cart_first_name"]')
+sendKeyXPath('Bot', '//*[@id="cart_last_name"]')
+sendKeyXPath('04'+phoneNumber, '//*[@id="cart_phone"]')
+sendKeyXPath(str(uuid.uuid4().hex), '//*[@id="cart_password"]')
+
+#select T&C
+clickXPath('//*[@id="booking"]/div/div[1]/div[2]/div/form/div[1]/div/div[3]/div/div/div/label/span[1]')
+
+#select BOOK & PAY
+clickXPath('//*[@id="booking"]/div/div[1]/div[2]/div/form/div[2]/div/button')
+time.sleep(7)
+
+#fill in credit card
+driver.switch_to_frame(frame_reference=driver.find_element_by_xpath('//iframe[@name="stripe_checkout_app"]'))
+card = driver.find_element_by_xpath('//input[@placeholder="Card number"]')
+card.send_keys('4242424242424242')
+
+cardDate = driver.find_element_by_xpath('//input[@placeholder="MM / YY"]')
+cardDate.send_keys('1221')
+
+cardCVC = driver.find_element_by_xpath('//input[@placeholder="CVC"]')
+cardCVC.send_keys('168')
+
+#select pay button
+clickXPath('//*[@id="container"]/section/span[2]/div/div/main/form/nav/div/div/div/button')
+
+time.sleep(20)
+driver.close()
